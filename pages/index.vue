1fb9e30da7aeb6e51e0d5ee8d872f465e5aca1b3
adiaskaputra <gadikpepe76@gmail.com>
2025-02-19 12:00:37 +0700

    chore: update package

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex items-center justify-between px-3 py-2">
+  <div class="custom flex items-center justify-between px-3 py-2">
     B O I L E R P L A T E
   </div>
 </template>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.custom {
+  @apply bg-green-100 text-gray-700;
+}
+</style>
